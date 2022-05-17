e0d9a64a97c41251f0bd7eb2ddfbffbc5ac07332
Georgina Scott <u7259296@anu.edu.au>
2022-05-17 18:44:17 +1000

    First changes, webcam implemented

--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+
+let video;
+var color_button;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background(10, 20, 40);
+
+  video = createCapture(VIDEO);
+  
+  color_button = createButton("Button");
+  color_button.position(400, 400);
+  
+
 }
 
-function draw() {
-  // put drawing code here
+function draw(){
+image(video, 20, 40, windowWidth, windowHeight);
+
+fill(random(20, 250), 200, random(180, 250));
+circle(200, 200, 90);
+
+
+
+  //color_button.mouseClicked(change_background)
+
 }
+
+function mousePressed(){
+circle(400, 400, 90).draw();
+
+}
+
+function change_background(){
+image(video, 20, 40, random(0, 40), random(0, 40));
+}
+
+

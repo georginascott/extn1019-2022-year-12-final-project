4da262257ab3d0808d9bc985335eb2c72fbc8a56
Georgina Scott <u7259296@anu.edu.au>
2022-08-02 19:01:06 +1000

    Latest - working, not moving though

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+var imgA, imgB, imgC, imgD, imgE, imgF, imgG, imgH;
 function preload(){
   imgA = loadImage('Letters/a.png');
   imgB = loadImage('Letters/b.png');
@@ -7,14 +8,24 @@ function preload(){
   imgF = loadImage('Letters/f.png');
   imgG = loadImage('Letters/g.png');
   imgH = loadImage('Letters/h.png');
-  imgI = loadImage('Letters/i.png');
-  imgJ = loadImage('Letters/j.png');
-  imgR = loadImage('Letters/r.png');
-  imgN = loadImage('Letters/n.png');
-  imgW = loadImage('Letters/w.png');
-  imgQ = loadImage('Letters/q.png');
-  imgT = loadImage('Letters/t.png');
-  imgY = loadImage('Letters/y.png');
+  // imgI = loadImage('Letters/i.png');
+  // imgJ = loadImage('Letters/j.png');
+  // imgK = loadImage('Letters/k.png');
+  // imgL = loadImage('Letters/l.png');
+  // imgM = loadImage('Letters/m.png');
+  // imgN = loadImage('Letters/n.png');
+  // imgO = loadImage('Letters/o.png');
+  // imgP = loadImage('Letters/p.png');
+  // imgQ = loadImage('Letters/q.png');
+  // imgR = loadImage('Letters/r.png');
+  // imgS = loadImage('Letters/s.png');
+  // imgT = loadImage('Letters/t.png');
+  // imgU = loadImage('Letters/u.png');
+  // imgV = loadImage('Letters/v.png');
+  // imgW = loadImage('Letters/w.png');
+  // imgX = loadImage('Letters/x.png');
+  // imgY = loadImage('Letters/y.png');
+  // imgZ = loadImage('Letters/z.png');
 }
 
 function setup(){
@@ -28,47 +39,34 @@ function setup(){
 }
   
 var count = 0;
-var letters = [count];
-var img = [24];
-var n;
-let yPos = 0;
-let aIs = false;
-let bIs = false;
+var letters = [];
+
 
 function keyPressed(){ 
   next = count * 90;
 
   if (keyCode === 65){ //a
     letters.push("a");
-     //newA = image(imgA, next, 400, 100, 100); //400 was 100 + next
-     //count++
      aIs = true;
   }
   if (keyCode === 66){ //b
     letters.push("a");
-     //newA = image(imgA, next, 400, 100, 100); //400 was 100 + next
-     //count++
      bIs = true;
   }
-  
 }
+
+var letterData;
 
 function draw(){
   frameRate(8); //was 0.02
   background(200, 200, 200);
-  yPos = yPos + 2;
-  //count = count * 20;
 
-  if (aIs == true){ //== for boolean
-    image(imgA, 100, yPos, 100, 100);
-    count++
-    //aIs = false; - can't have this as the the letter will go away when the aIs becomes false
-  }
-  if (bIs == true){
-    image(imgB, 100, yPos, 100, 100);
-    count++
-  }
-  //PROBLEM HERRE: WE CANNOT SPAWN THE LETTERS AFTER EACH OTHER, PROBLEMATIC ATM!
+ //loop through array of letters, 
+ for(var i = 0; i < letters.length; i++){
+   let letterData = letters[i]
+   console.log(letterData);
+   image(letterData[0], letterData[1], letterData[2], letterData[3], letterData[4]);
+ }
 }
 
 function reassign(){
@@ -82,114 +80,173 @@ var count = count;
 var y = 20;
 var newA;
 
+function keyPressed(){ 
+   next = count * 90;
 
-
-
-
-
-// function keyPressed(){ 
-//   next = count * 90;
-
-//   if (keyCode === 65){ //a
-//     letters.push("a");
-//      //newA = image(imgA, next, 400, 100, 100); //400 was 100 + next
-//      count++
-//      aIs = true;
-//   }
+  if (keyCode === 65){ //a
+    let y = random(100, 200);
+    letters.push([imgA, next, y, 100, 100]);
+    //image(imgA, next, 100 + random(1, 100), 100, 100);
+    count++
+  }
   
-//   if (keyCode === 66){ //b
-//     letters.push("b");
-//      image(imgB, next, 400, 100, 100);
-//      count++
-//   }
+  if (keyCode === 66){ //b
+    let y = random(100, 200);
+    letters.push([imgB, next, y, 100, 100]);
+    //image(imgB, next, 100 + random(1, 100), 100, 100);
+     count++
+  }
 
-//   if (keyCode === 67){ //c
-//     letters.push("c");
-//      image(imgC, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 67){ //c
+  //   let y = random(100, 200);
+  //   letters.push(["c", next, y]);
+  //    image(imgC, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 68){ //d
-//     letters.push("d");
-//      image(imgD, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 68){ //d
+  //   let y = random(100, 200);
+  //   letters.push(["d", next, y]);
+  //    image(imgD, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 69){ //e
-//     letters.push("e");
-//      image(imgE, next, 100 + next, 100, 100);
-//      count++
-//   }
+  
+  // if (keyCode === 69){ //e
+  //   let y = random(100, 200);
+  //   letters.push(["e", next, y]); //passes back 3 element array when called
+  //    image(imgE, next, y, 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 70){ //f
-//     letters.push("f");
-//      image(imgF, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 70){ //f
+  //   let y = random(100, 200);
+  //   letters.push(["f", next, y]);
+  //    image(imgF, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 71){ //g
-//     letters.push("g");
-//      image(imgG, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 71){ //g
+  //   letters.push("g");
+  //    image(imgG, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 72){ //h
-//     letters.push("h");
-//      image(imgH, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 72){ //h
+  //   letters.push("h");
+  //    image(imgH, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 73){ //i
-//     letters.push("i");
-//      image(imgI, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 73){ //i
+  //   letters.push("i");
+  //    image(imgI, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 74){ //j
-//     letters.push("j");
-//      image(imgJ, next, 100 + next, 100, 100);
-//      count++
-//   }
+  // if (keyCode === 74){ //j
+  //   letters.push("j");
+  //    image(imgJ, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
+  // if (keyCode === 75){ //k
+  //   letters.push("k");
+  //    image(imgK, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 78){ //n
-//     letters.push("n");
-//     image(imgN, next, y, 100, 100); //y = 150
-//     count++ 
-//   }
+  // if (keyCode === 76){ //l
+  //   letters.push("l");
+  //    image(imgL, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 81){ //q
-//     letters.push("q")
-//     image(imgQ, next, 190, 100, 200);
-//     count++
-//   }
+  // if (keyCode === 77){ //m
+  //   letters.push("m");
+  //    image(imgM, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 82){ //r
-//     letters.push("r");
-//     image(imgR, next, 200, 100, 100);
-//     count++
-//   }
+  // if (keyCode === 78){ //n
+  //   letters.push("n"); //push a 3 element array; letter, x, y
+  //   image(imgN, next, 100 + random(1, 100), 100, 100); //y = 150
+  //   count++ 
+  // }
 
-//   if (keyCode === 84){ //t
-//     letters.push("t");
-//     image(imgT, next, 280, 100, 100);
-//     count++
-//   }
+  // if (keyCode === 79){ //o
+  //   letters.push("o");
+  //    image(imgO, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 87){ //w
-//     letters.push("w")
-//     image(imgW, next, 360, 100, 200);
-//     count++
-//   }
+  // if (keyCode === 80){ //p
+  //   letters.push("p");
+  //    image(imgP, next, 100 + random(1, 100), 100, 100);
+  //    count++
+  // }
 
-//   if (keyCode === 89){ //y
-//     letters.push("y")
-//     image(imgY, next, 400, 100, 200);
-//     count++
-//   }
-//   console.log(count)
-//   count = count;
-// }
+  // if (keyCode === 81){ //q
+  //   letters.push("q")
+  //   image(imgQ, next, 100 + random(1, 100), 100, 200);
+  //   count++
+  // }
+
+  // if (keyCode === 82){ //r
+  //   letters.push("r");
+  //   image(imgR, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+
+  // if (keyCode === 83){ //s
+  //   letters.push("s");
+  //   image(imgS, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+
+  // if (keyCode === 84){ //t
+  //   letters.push("t");
+  //   image(imgT, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+  // if (keyCode === 85){ //u
+  //   letters.push("u");
+  //   image(imgU, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+
+  // if (keyCode === 86){ //v
+  //   letters.push("v");
+  //   image(imgV, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+
+  // if (keyCode === 87){ //w
+  //   letters.push("w")
+  //   image(imgW, next, 100 + random(1, 100), 100, 200);
+  //   count++
+  // }
+
+  // if (keyCode === 88){ //x
+  //   letters.push("x");
+  //   image(imgX, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+
+  // if (keyCode === 89){ //y
+  //   letters.push("y")
+  //   image(imgY, next, 100 + random(1, 100), 100, 200);
+  //   count++
+  // }
+  // console.log(count)
+  // count = count;
+
+  // if (keyCode === 90){ //z
+  //   letters.push("z");
+  //   image(imgZ, next, 100 + random(1, 100), 100, 100);
+  //   count++
+  // }
+}
 
 
 
